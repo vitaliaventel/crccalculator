324d905a1434fe4a277fb87f509427b301ccc572
vitaliaventel <vitaliaventel@gmail.com>
2016-12-11 13:46:12 +0200

    Updated UI, added radiobuttons for CRC16/32

--- src/ua/kpi/crc/CRCApplication.java
+++ src/ua/kpi/crc/CRCApplication.java
@@ -1,11 +1,14 @@
 package ua.kpi.crc;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,6 +18,9 @@ public class CRCApplication extends Application {
 	private Button button;
 	private TextField inputField;
 	private Label label;
+	private final ToggleGroup group = new ToggleGroup();
+	private RadioButton rb1;
+	private RadioButton rb2;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -29,21 +35,30 @@ public class CRCApplication extends Application {
 			windowClose();
 		});
 
+		rb1 = new RadioButton("CRC16");
+		rb1.setToggleGroup(group);
+		rb1.setSelected(true);
+
+		rb2 = new RadioButton("CRC32");
+		rb2.setToggleGroup(group);
+
 		label = new Label();
 
 		inputField = new TextField();
-		inputField.setPromptText("Hex value");
+		inputField.setPromptText("String data");
 		inputField.setMinWidth(60);
 
-		button = new Button("Calculate");
+		button = new Button("Calculate!");
 		button.setOnAction(e -> {
-
+			String result = radioSelect();
+			setLabel("For message: " + inputField.getText() + ", CRC = " + result);
 		});
 
 		VBox lay = new VBox();
-		lay.getChildren().addAll(inputField, button, label);
+		lay.setPadding(new Insets(10, 10, 10, 10));
+		lay.getChildren().addAll(rb1, rb2, inputField, button, label);
 		lay.setAlignment(Pos.CENTER);
-		Scene scene = new Scene(lay, 200, 150);
+		Scene scene = new Scene(lay, 300, 150);
 		window.setScene(scene);
 		window.show();
 	}
@@ -53,9 +68,17 @@ public class CRCApplication extends Application {
 		if (answer)
 			window.close();
 	}
-	
-	public void setLabel(String text){
+
+	private void setLabel(String text) {
 		label.setText(text);
+	}
+
+	private String radioSelect() {
+		if (rb1.isSelected()) {
+			return CRCAlghoritm.crc16(inputField.getText());
+		} else {
+			return CRCAlghoritm.crc32(inputField.getText());
+		}
 	}
 
 }
